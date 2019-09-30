52552bfc7ea33b8cfac91258b570c79c24c71e09
personalc <personalc@163.com>
2019-09-30 16:19:30 +0800

    Update flink-yarn.sh

--- flink-parcel-src/flink-yarn.sh
+++ flink-parcel-src/flink-yarn.sh
@@ -15,7 +15,9 @@ log_setting="-Dlog.file="$log" -Dlog4j.configuration=file:"$FLINK_CONF_DIR"/log4
 
 # Rotate log files
 rotateLogFilesWithPrefix "$FLINK_LOG_DIR" "$FLINK_LOG_PREFIX"
-CLASS_PATH=`manglePathList $(constructFlinkClassPath):$(hadoop classpath)`
-CLASS_PATH=$(echo "${CLASS_PATH}" | sed "s#"$FLINK_HOME"/lib/slf4j-log4j12-1.7.7.jar:##g")
+CLASS_PATH=`manglePathList $(constructFlinkClassPath):$INTERNAL_HADOOP_CLASSPATHS`
+#CLASS_PATH=`manglePathList $(constructFlinkClassPath):$(hadoop classpath)`
+#CLASS_PATH=$(echo "${CLASS_PATH}" | sed "s#"$FLINK_HOME"/lib/slf4j-log4j12-1.7.7.jar:##g")
 exec $JAVA_RUN $JVM_ARGS -classpath "$CLASS_PATH" $log_setting ${CLASS_TO_RUN} -j "$FLINK_LIB_DIR"/flink-dist*.jar "$@" > "$out" 2>&1
+
 
